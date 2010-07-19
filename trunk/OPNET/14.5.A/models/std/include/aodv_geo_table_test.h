dc4a2c648bf336755135387455be27ea314ac877
remococco <remococco@d5dceeee-80e2-11dd-ac6e-7909132840ff>
2010-07-19 22:59:38 +0000

    Added seperate files for geo table tests and fixed several errors.

---
+++ trunk/OPNET/14.5.A/models/std/include/aodv_geo_table_test.h
@@ -0,0 +1,33 @@
+// 07/07/2010 RC
+// Test funnctions for the table
+// NOTE: THESE TEST FUNCTIONS ARE MEANT TO BE DELETED ONCE 
+// THE FUNCTIONALITY OF THE TABLE HAS BEEN CONFIRMED
+/* aodv_geo_table_test.c
+ * Test the implementation of aodv_geo_table
+ * for use in the geo aodv routing protocol.
+ *
+ * Author: Remo Cocco
+ */
+
+#include <opnet.h>
+#include <aodv.h>
+#include <aodv_ptypes.h>
+#include <math.h>
+
+#define _ENTRY_EXISTS_TESTS 20
+#define _UPDATE_TESTS 20
+
+typedef struct InetT_Address_Helper
+{
+	InetT_Address *inett_address;
+	double x;
+	double y;
+} InetT_Address_Helper;
+
+void aodv_geo_table_test();
+
+InetT_Address* generate_random_InetT_Address();
+
+void aodv_geo_table_entry_exists_test(AodvT_Geo_Table*);
+void aodv_geo_table_update_test(AodvT_Geo_Table*);
+
