7c309dfaff76436d7360e6bc7d71db43ea3acea5
remococco <remococco@d5dceeee-80e2-11dd-ac6e-7909132840ff>
2010-11-11 16:24:36 +0000

    Edited name to match version 1.1 design spec.

---
+++ trunk/LARTest/LARTestFramework/src/InvalidRequestZoneException.py
@@ -0,0 +1,13 @@
+"""
+This class should be thrown when an invalid
+LARContainmentStrategy is supplied to a function
+
+Author: Remo Cocco
+"""
+
+
+class InvalidRequestZoneException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
