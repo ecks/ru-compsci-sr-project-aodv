c55483af85aa708d1689968aab692d879b047016
animanmaster <animanmaster@d5dceeee-80e2-11dd-ac6e-7909132840ff>
2010-11-24 04:50:44 +0000

    Finished implementing LAR update interrupt and verified correct functionality. If you have linking problems, add the linking flag /FORCE when running the simulation (to FORCE OPNET to STOP COMPLAINING!).
    Notes:
    - To turn off debug messages, comment out #define LAR_DEBUG in aodv_geo_support.h
    - While implementing code in aodv_geo_support.ex.c, surround debug prints with #ifdef LAR_DEBUG.
    - Instead of the node's IP, the node's name is used as the key for the database. So, if you want to access the LAR_Data struct to retrieve the last updated location and velocity information, get the node's name using
    op_ima_obj_attr_get_str_sized(node_id, "name", &name_size, name) 
    where name is a char[NAME_ATTR_SIZE], and then extract the LAR_Data pointer from the database using 
    data = oms_data_def_entry_access(LAR_OMS_CATEGORY, name);

--- trunk/OPNET/14.5.A/models/std/manet/aodv_rte.pr.c
+++ trunk/OPNET/14.5.A/models/std/manet/aodv_rte.pr.c
@@ -15,7 +15,7 @@
 
 
 /* This variable carries the header into the object file */
-const char aodv_rte_pr_c [] = "MIL_3_Tfile_Hdr_ 160A 30A op_runsim_dev 7 4CEAF0BA 4CEAF0BA 1 Robilablap-00 student 0 0 none none 0 0 none 0 0 0 0 0 0 0 0 277a 1                                                                                                                                                                                                                                                                                                                                                                                              ";
+const char aodv_rte_pr_c [] = "MIL_3_Tfile_Hdr_ 160A 30A op_runsim_dev 7 4CEC73D0 4CEC73D0 1 Robilablap-00 student 0 0 none none 0 0 none 0 0 0 0 0 0 0 0 277a 1                                                                                                                                                                                                                                                                                                                                                                                              ";
 #include <string.h>
 
 
